76495286ab20c755c82d07e56238cd8852e7e872
thebigjeffy9 <sawong@s.sfusd.edu>
2025-10-31 15:36:15 -0700

    Module 7 lesson 6 completed

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -1,19 +1,20 @@
 extends Control
 
-@onready var rich_text_label: RichTextLabel = %RichTextLabel
-@onready var next_button: Button = %NextButton
 
-## An array of strings. Each string contains the text we want the character to
-## say.
 var dialogue_items: Array[String] = [
 	"Hi I'm Gavin",
 	"My football fantasy is trash",
-	" And I had to quit because I suck",
-	" Its all good",
+	"And I had to quit because I suck",
+	"Its all good",
 	"Bye",
 ]
-## Holds the index of the currently displayed text
+
 var current_item_index := 0
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+
 
 func _ready() -> void:
 	show_text()
@@ -23,11 +24,21 @@ func _ready() -> void:
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item
+	
+	rich_text_label.visible_ratio = 0.0
+	var tween := create_tween()
+	var text_appearing_duration := 1.2
+	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+	
+	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
+	var sound_start_position := randf() * sound_max_offset
+	audio_stream_player.play(sound_start_position)
+	
+	tween.finished.connect(audio_stream_player.stop)
 
 func advance() -> void:
 	current_item_index += 1
-	# If we reached the end of the dialogue, we quit the game. Otherwise, we
-	# show the next dialogue line.
+	
 	if current_item_index == dialogue_items.size():
 		get_tree().quit()
 	else:
