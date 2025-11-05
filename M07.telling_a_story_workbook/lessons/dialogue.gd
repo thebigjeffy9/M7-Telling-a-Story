a3c3fab1d0d096c1ea00789d467e23bcce5c6fbd
thebigjeffy9 <sawong@s.sfusd.edu>
2025-11-05 13:22:10 -0800

    Module 7 lesson 10 completed

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -1,44 +1,76 @@
 extends Control
 
+var expressions := {
+	"happy": preload ("res://assets/emotion_happy.png"),
+	"regular": preload ("res://assets/emotion_regular.png"),
+	"sad": preload ("res://assets/emotion_sad.png"),
+}
 
-var dialogue_items: Array[String] = [
-	"Hi I'm Gavin",
-	"My football fantasy is trash",
-	"And I had to quit because I suck",
-	"Its all good",
-	"Bye",
+## An array of dictionaries. Each dictionary has two properties:
+## - expression: a [code]Texture[/code] containing an expression
+## - text: a [code]String[/code] containing the text the character says
+var dialogue_items: Array[Dictionary] = [
+	{
+		"expression": expressions["regular"],
+		"text": "Hi I'm Gavin",
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "My football fantasy is trash.",
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "And I had to quit because I suck",
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "Its all good",
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "bye",
+	},
 ]
-
 var current_item_index := 0
 
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
 
 
 func _ready() -> void:
 	show_text()
 	next_button.pressed.connect(advance)
 
+
 ## Draws the current text to the rich text element
 func show_text() -> void:
+	# We retrieve the current dictionary from the array and assign its
+	# properties to the UI elements.
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
-	
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
+	# We animate the text appearing letter by letter.
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration := 1.2
+	var text_appearing_duration := 1.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
-	
+
+	# This is where we play the audio. We randomize the audio playback's start
+	# time to make it sound different every time.
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
-	
+	# We stop the audio when the text finishes appearing.
 	tween.finished.connect(audio_stream_player.stop)
+
 
 func advance() -> void:
 	current_item_index += 1
-	
+	# If we reached the end of the dialogue, we quit the game. Otherwise, we
+	# show the next dialogue line.
 	if current_item_index == dialogue_items.size():
 		get_tree().quit()
 	else:
