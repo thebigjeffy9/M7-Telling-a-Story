fd36ae6e4d9a5b36df3134d3387b069318986285
thebigjeffy9 <sawong@s.sfusd.edu>
2025-11-05 13:40:51 -0800

    Module 7 lesson 11 completed

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -6,29 +6,27 @@ var expressions := {
 	"sad": preload ("res://assets/emotion_sad.png"),
 }
 
-## An array of dictionaries. Each dictionary has two properties:
-## - expression: a [code]Texture[/code] containing an expression
-## - text: a [code]String[/code] containing the text the character says
+
 var dialogue_items: Array[Dictionary] = [
 	{
 		"expression": expressions["regular"],
-		"text": "Hi I'm Gavin",
+		"text": "I'm learning about Arrays...",
 	},
 	{
 		"expression": expressions["sad"],
-		"text": "My football fantasy is trash.",
+		"text": "... and it is a little bit complicated.",
 	},
 	{
 		"expression": expressions["happy"],
-		"text": "And I had to quit because I suck",
+		"text": "Let's see if I got it right: an array is a list of values!",
 	},
 	{
 		"expression": expressions["regular"],
-		"text": "Its all good",
+		"text": "Did I get it right? Did I?",
 	},
 	{
 		"expression": expressions["happy"],
-		"text": "bye",
+		"text": "Hehe! Bye bye~!",
 	},
 ]
 var current_item_index := 0
@@ -47,31 +45,40 @@ func _ready() -> void:
 
 ## Draws the current text to the rich text element
 func show_text() -> void:
-	# We retrieve the current dictionary from the array and assign its
-	# properties to the UI elements.
+	
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item["text"]
 	expression.texture = current_item["expression"]
-	# We animate the text appearing letter by letter.
+	
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
 	var text_appearing_duration := 1.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 
-	# This is where we play the audio. We randomize the audio playback's start
-	# time to make it sound different every time.
+	
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
-	# We stop the audio when the text finishes appearing.
+	
 	tween.finished.connect(audio_stream_player.stop)
+
+	slide_in()
 
 
 func advance() -> void:
 	current_item_index += 1
-	# If we reached the end of the dialogue, we quit the game. Otherwise, we
-	# show the next dialogue line.
+	
 	if current_item_index == dialogue_items.size():
 		get_tree().quit()
 	else:
 		show_text()
+
+
+func slide_in() -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_OUT)
+	body.position.x = 200.0
+	tween.tween_property(body, "position:x", 0.0, 0.3)
+	body.modulate.a = 0.0
+	tween.parallel().tween_property(body, "modulate:a", 1.0, 0.2)
